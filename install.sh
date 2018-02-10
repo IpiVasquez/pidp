ec91637eaccf5bcf8cef781600ef8560631b6211
Hector J. Vasquez <ipi.vasquez@gmail.com>
2018-02-10 11:57:15 -0600

    Install update

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 npm i
 cp -R src/types/opencv node_modules/@types/
+cp -R node_modules/opencv/build/opencv/v6.0.0/Release node_modules/opencv/build/opencv/v6.0.0/Debug
 mkdir tmp
 npm run build
 npm start
